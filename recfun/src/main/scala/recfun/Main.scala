318d055da9f10f1e448cc8d7af135904f240d2fc
marnax <marnax@gmx.de>
2016-06-07 22:16:41 +0200

    graded

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,57 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int =
+    if (r < 0 || c < 0 || c > r) {
+      0
+    }
+    else if (r == 0) {
+      1
+    }
+    else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
+
   /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def innerBalance(chars: List[Char], count: Integer): Integer =
+      if (chars.isEmpty || count < 0)
+        count
+      else if (chars.head.equals('('))
+        innerBalance(chars.tail, count + 1)
+      else if (chars.head.equals(')'))
+        innerBalance(chars.tail, count - 1)
+      else
+        innerBalance(chars.tail, count)
+    innerBalance(chars, 0) == 0
   }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def innerCount(money: Int, coins: List[Int]): Int = {
+      if (money < 0 || coins.isEmpty) {
+        0
+      }
+      else if (money == coins.head) {
+        1 + innerCount(money, coins.tail)
+      }
+      else {
+        innerCount(money - coins.head, coins) + innerCount(money, coins.tail)
+      }
+    }
+    if (money == 0) {
+      1
+    }
+    else if (money < 0 || coins.isEmpty) {
+      0
+    }
+    else
+      innerCount(money, coins)
+  }
+}
